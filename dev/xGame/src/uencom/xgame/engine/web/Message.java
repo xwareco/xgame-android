d36ee11667e3d8321a190cb8166e402ed8e88429
AlyMoanes <a.moanes@xware.co>
2015-08-31 11:49:56 +0200

    dev bransh created

---
+++ dev/xGame/src/uencom/xgame/engine/web/Message.java
@@ -0,0 +1,28 @@
+package uencom.xgame.engine.web;
+
+public class Message {
+
+	private String id;
+	private String user_id;
+	private String body;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+	
+}
